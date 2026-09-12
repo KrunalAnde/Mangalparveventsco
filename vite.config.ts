c19713e086d89ebdd5fbf2c18d54932a9935d065
KrunalAnde <115775011+KrunalAnde@users.noreply.github.com>
2026-09-12 23:07:59 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Mangalparveventsco/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
